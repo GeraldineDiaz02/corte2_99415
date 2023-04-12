1c1a69b62f413b4a2d6f04b4596839893ba474a8
GeraldineDiaz02 <jinethdiaz02@gmail.com>
2023-04-11 19:08:10 -0500

    Tarea sesion 12

--- tarea4iva.py
+++ tarea4iva.py
@@ -1,30 +1,16 @@
-def calcular_iva(producto, valor_neto):
+def calcularIva(producto, valor_neto):
     with open("Alimentos.txt", "r") as f:
         contenido = [linea.strip().split(",") for linea in f.readlines()]
 
     for fila in contenido:
-        if fila[0] == producto:
-            nombre = fila[1]
-            porcentaje_iva = float(fila[2])
-            valor_base = valor_neto / (1 + porcentaje_iva)
-            valor_iva = valor_base * porcentaje_iva
-            return (nombre, valor_base, valor_iva)
+        if fila[1] == producto:
+            nombre = fila[0]
+            porcentajeIva = float(fila[2])
+            valorBase = valor_neto / (1 + porcentajeIva)
+            valorIva = valorBase * porcentajeIva
+            return (nombre, valorBase, valorIva)
 
     return None
 
-while True:
-    producto = input("Ingrese el código del producto o escriba 'salir' para terminar: ")
-    if producto == "salir":
-        break
-
-    valor_neto = float(input("Ingrese el valor neto del producto: "))
-    resultado = calcular_iva(producto, valor_neto)
-
-    if resultado is None:
-        print("El código del producto no fue encontrado.")
-    else:
-        nombre, valor_base, valor_iva = resultado
-        print("Producto: {}".format(nombre))
-        print("Valor base: ${:,.2f}".format(valor_base))
-        print("IVA ({:.0%}): ${:,.2f}".format(valor_iva / valor_base, valor_iva))
-        print("Valor bruto: ${:,.2f}\n".format(valor_neto))
+if __name__ == "__main__":
+    calcularIva()
