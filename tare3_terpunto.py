bfcfde7af81cd0dafd2fd60fe4fb89ddb16740de
GeraldineDiaz02 <125599050+GeraldineDiaz02@users.noreply.github.com>
2023-04-19 05:44:18 -0500

    Delete tare3_terpunto.py

--- tare3_terpunto.py
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    def factorial_aux(n, acc):
-        if n == 0:
-            return acc
-        else:
-            return factorial_aux(n-1, n*acc)
-        
-    return factorial_aux(n, 1)
-
